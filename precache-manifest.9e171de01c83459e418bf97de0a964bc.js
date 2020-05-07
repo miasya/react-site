bf3b6fa39399cde523897945fdb812a646746ba2
miasya <41090034+miasya@users.noreply.github.com>
2020-05-07 19:22:15 -0400

    Updates

---
+++ precache-manifest.9e171de01c83459e418bf97de0a964bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f1dfe64f3b8808d035dd3f8e9c62078",
+    "url": "/react-site/index.html"
+  },
+  {
+    "revision": "1c6a8d1e41c5c8c8a200",
+    "url": "/react-site/static/css/main.25d2ff5d.chunk.css"
+  },
+  {
+    "revision": "3e5ca8bec54dca80fed5",
+    "url": "/react-site/static/js/2.db6aee81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-site/static/js/2.db6aee81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c6a8d1e41c5c8c8a200",
+    "url": "/react-site/static/js/main.911cdb4f.chunk.js"
+  },
+  {
+    "revision": "d3f845b8d90ee44310f8",
+    "url": "/react-site/static/js/runtime-main.6796ed9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-site/static/media/logo.5d5d9eef.svg"
+  }
+]);
